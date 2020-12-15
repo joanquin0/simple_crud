3247b231adee64b0c1ed22160eaed5f661617974
joanquin0 <72197121+joanquin0@users.noreply.github.com>
2020-12-16 01:00:13 +0800

    comit

--- src/context/AppReducer.js
+++ src/context/AppReducer.js
@@ -1,2 +1,12 @@
 import React, { createContext, userReducer } from 'react';
-import 
+import AddReducer from'./AppReducer';
+
+const initialState = {
+     user:[
+          {id: 1, name: 'User One'},
+          {id: 2, name: 'User Two'},
+          {id: 3, name: 'User Three'}
+     ]
+}
+export const GlobalContext = createContext(initialState);
+export const GlobalProvider = ()
